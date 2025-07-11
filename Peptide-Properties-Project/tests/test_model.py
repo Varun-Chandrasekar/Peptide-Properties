35fd3cc5599f30948a42bec79f4b60edcf147004
Varun-Chandrasekar <78281503+Varun-Chandrasekar@users.noreply.github.com>
2025-07-10 17:34:48 -0700

    Update test_model.py

--- Peptide-Properties-Project/tests/test_model.py
+++ Peptide-Properties-Project/tests/test_model.py
@@ -1,14 +1,14 @@
 """
 test_model.py
 
-Basic unit test for the PeptideRegressor model.
+Basic unit test for the model.
 """
 
 import torch
-from peptide_properties.model import PeptideRegressor
+from peptide_properties.model import CNN
 
 def test_model_output_shape():
-    model = PeptideRegressor(input_dim=20)
+    model = CNN(input_dim=20)
     dummy_input = torch.randint(0, 20, (4, 50))  # batch_size=4, sequence_len=50
     output = model(dummy_input)
     assert output.shape == (4, 3), f"Expected output shape (4, 3), but got {output.shape}"
